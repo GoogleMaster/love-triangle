df7d4a2eafc7fb67af55d0f4663d2fc5be306f0b
Kjudisca Rasjudisca <xaneta@ya.ru>
2018-02-20 16:07:10 +0300

    calculating function now works fine

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var count = 0;
+    for (var i = 0; i < preferences.length; i++) {
+        var first = preferences[i];
+        var second = preferences[first - 1];
+        if (preferences[second - 1] == i + 1) {
+            count++;
+        }
+    }
+    return Math.floor(count / 3);
 };
